190ff133907a6990642322a109643e16d423fdf0
JonMize <mizejonathanl@gmail.com>
2024-11-09 21:57:41 -0500

    fix homepage style

--- homePage.php
+++ homePage.php
@@ -3,9 +3,18 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
     <title>Homepage</title>
 </head>
 <body>
-    
+    <div class="homepage-title-container">
+        <div class="homepage-title">GSU HangMan</div>
+    </div>
+    <div class="homepage-buttons-container">
+        <div class="homepage-button">Play</div>
+        <div class="homepage-button">Difficulty</div>
+        <div class="homepage-button">Stats</div>
+        <div class="homepage-button">Reset Stats</div>
+    </div>
 </body>
 </html>
